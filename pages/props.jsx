7bc859689e71d2213af9bc74c133537d5598d7d8
Akhmad Salafudin <axmad386@gmail.com>
2022-03-13 07:11:31 +0700

    fix: hapus context yang tidak terpakai

--- pages/props.jsx
+++ pages/props.jsx
@@ -1,22 +1,16 @@
-import { useEffect, useState } from "react";
 import Counter from "../component/Counter"
 import CounterClass from "../component/CounterClass"
 import Layout from "../component/Layout"
-import { ThemeContext } from "../context/ThemeContext";
 
 const Props = ()=>{
 
     
 
     return (
-    <Layout dark={dark} toggleDarkMode={toggleDarkMode}>
-        <ThemeContext.Provider value={{
-            dark: dark
-        }}>
+    <Layout>
             <h1>Belajar Props</h1>
             <Counter label="HITUNG" jumlahAwal={100}/>
             <CounterClass label="HITUNG CLASS"/>
-        </ThemeContext.Provider>
     </Layout>
 
     )
